3c6348b77f15639be94f3ee60b9e3b2deb5a76e0
Spencer Lott <spklott@gmail.com>
2023-03-14 09:44:55 -0400

    completed chapters 1-9

---
+++ scripts/Orders.js
@@ -0,0 +1,66 @@
+import { getCustomOrders, getMetals, getSizes, getStyles } from "./database.js"
+
+const metals = getMetals()
+const styles = getStyles()
+const sizes = getSizes()
+
+const buildOrderListItem = (order) => {
+    
+    // Remember that the function you pass to find() must return true/false
+    const foundMetal = metals.find(
+        (metal) => {
+            
+            return metal.id === order.metalId}
+    )
+   
+
+    const foundStyle = styles.find(
+        (style) => {
+
+            return style.id === order.styleId}
+    )
+    const nameOfStyle = foundStyle.style
+    
+
+    const styleString = nameOfStyle.toLocaleString("") 
+ 
+    const foundSize = sizes.find(
+        (size) => {
+
+            return size.id === order.sizeId}
+    )
+
+
+const totalCost = foundMetal.price + foundStyle.price + foundSize.price
+
+const costString = totalCost.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD"
+}) 
+   return `<li>
+        Order #${order.id} cost ${costString} and is a ${styleString} style.
+    </li>`
+
+
+
+}
+
+
+export const Orders = () => {
+    /*
+        Can you explain why the state variable has to be inside
+        the component function for Orders, but not the others?
+    */
+
+    const orders = getCustomOrders()
+
+    let html = "<ul>"
+
+    const listItems = orders.map(buildOrderListItem)
+
+    html += listItems.join("")
+    html += "</ul>"
+
+    return html
+}
+
